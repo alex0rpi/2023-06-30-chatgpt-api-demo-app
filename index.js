0c8694653a0394f1c91c8b4421bd0ce8da4287de
alex0rpi <orpi.alex@hotmail.com>
2023-07-03 19:35:10 +0200

    should work, only payed account needed

--- index.js
+++ index.js
@@ -6,14 +6,23 @@ async function main() {
   console.log(colors.bold.green('Benvingut a aquesta aplicació Chatbot!'));
   console.log(colors.bold.green('Ja pots començar a xatejar amb el bot🤖!'));
 
+  const chatHistory = []; //store conversation history
+
   while (true) {
     const userInput = readlineSync.question(colors.yellow('Tu: '));
 
     try {
+      // Construct messages by iterating over the history
+      const messages = chatHistory.map(([role, content]) => ({ role, content }));
+
+      //   Add latest user input
+      messages.push({ role: 'user', content: userInput });
+
       // Call the API with the user's input
       const completion = openai.createChatCompletion({
         model: 'gpt-3.5-turbo',
-        messages: [{ role: 'user', content: userInput }],
+        // We pass all the messages made to the bot, so it can answer further question with the previous messages in mind.
+        messages: messages,
       });
 
       // Get chatbot completion text
@@ -25,6 +34,10 @@ async function main() {
       }
 
       console.log(colors.green('Bot🤖: ') + completionText);
+
+      //   Update history with user input and bot response
+      chatHistory.push(['user', userInput]);
+      chatHistory.push(['bot', completionText]);
     } catch (error) {
       console.error(colors.red(error));
     }
