25ee6c07459d24ee7fb9c2000711fa29f463e30a
alex0rpi <orpi.alex@hotmail.com>
2023-07-03 19:29:28 +0200

    Api not working

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// import openai from './config/open-ai';
+import openai from './config/open-ai.js';
 import readlineSync from 'readline-sync';
 import colors from 'colors';
 
@@ -11,10 +11,20 @@ async function main() {
 
     try {
       // Call the API with the user's input
+      const completion = openai.createChatCompletion({
+        model: 'gpt-3.5-turbo',
+        messages: [{ role: 'user', content: userInput }],
+      });
 
-      if (userInput === 'exit') {
-        break;
+      // Get chatbot completion text
+      const completionText = (await completion).data.choices[0].message.content;
+
+      if (userInput.toLocaleLowerCase() === 'exit') {
+        console.log(colors.green('Bot🤖: ') + completionText);
+        return;
       }
+
+      console.log(colors.green('Bot🤖: ') + completionText);
     } catch (error) {
       console.error(colors.red(error));
     }
---
+++ readme.md
@@ -0,0 +1,6 @@
+### Simple console chatgpt bot app.
+
+Please create a .env file and fill it with the fields specified in .env-test.
+Make sure not to share your openai api key.
+
+To this day, seems one needs a payed account to have this api work. Otherwise it throus a 429 error.
